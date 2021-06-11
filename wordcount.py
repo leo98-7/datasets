2d7b670e1799a2b61383191053b4673f9260937f
shield-code <85719904+shield-code@users.noreply.github.com>
2021-06-11 13:24:45 +0800

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+Class txtBreakdown(MRJob):
+    def setps(self):
+        return  []
+            MRStep(mapper= self.mapper,
+                   reducer()=self.reducer)
+
+def mapper(self, _, line):
+    word = line.split()
+    yield word, 1
+
+
+def reducer(self, key, values):
+    yield key, sum(values)
+    
+if __name__== '__main__':
+    txtBreakdown.run()
